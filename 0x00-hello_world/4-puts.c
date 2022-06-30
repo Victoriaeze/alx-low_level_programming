21360ab0a6c7512debceca22706526bbf88e1b0e
Victoriaeze <noblequeen048@gmail.com>
2022-06-30 15:46:55 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-/
-int main(){ 
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+#include <studio.h>
+
+int main(void)
+{
+  puts("/"programing is lime a multilingual puzzle");
+Return (0);
 }
