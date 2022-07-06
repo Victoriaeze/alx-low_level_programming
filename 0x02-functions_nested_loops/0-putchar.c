9b74e3e0db0cfc8fcc118ca2c7dc92c427e1493a
Victoriaeze <noblequeen048@gmail.com>
2022-07-05 20:59:56 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - a function that prints _putchar
+ *
+ * Return: return 0 as success
+ *
+ */
+
+int main(void)
+{
+	char character[] = "_putchar";
+	int i;
+	int lenght;
+
+	lenght = sizeof(character) / sizeof(character[0]);
+
+	for (i = 0; i < lenght - 1; i++)
+	{
+		_putchar(character[i]);
+
+	}
+		_putchar('\n');
+
+		return (0);
+
+
+}
